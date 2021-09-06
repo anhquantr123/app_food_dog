24793c84003be7f1c94d2c82a3865f8c5ea34286
NGUYỄN VĂN QUÂN <anhquan.lien123@gmail.com>
2021-09-06 23:51:06 +0700

    add bottom tab and screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:app_food/constants/constants.dart';
+import 'package:app_food/screens/account/account-screen.dart';
+import 'package:app_food/screens/chat/chat-screen.dart';
 import 'package:app_food/screens/home/home_screen.dart';
+import 'package:app_food/screens/notification/notification-screen.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int indexScreen = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,9 +29,46 @@ class MyApp extends StatelessWidget {
               bodyText1: TextStyle(color: textColorBlack),
               bodyText2: TextStyle(color: textColorLightBlack))),
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: Scaffold(
+        body: Screen(),
+        bottomNavigationBar: bottomTapNavigation(),
+      ),
     );
   }
-  // This widget is the root of your application.
 
+  // ignore: non_constant_identifier_names
+  Screen() {
+    switch (indexScreen) {
+      case 0:
+        return HomeScreen();
+      case 1:
+        return ChatScreen();
+      case 2:
+        return NotificationScreen();
+      case 3:
+        return AccountScreen();
+      default:
+        return HomeScreen();
+    }
+  }
+
+  Widget bottomTapNavigation() {
+    return BottomNavyBar(
+        selectedIndex: indexScreen,
+        items: [
+          BottomNavyBarItem(
+              icon: SvgPicture.asset("assets/icons/home.svg"),
+              title: Text("Home")),
+          BottomNavyBarItem(
+              icon: SvgPicture.asset("assets/icons/chat.svg"),
+              title: Text("Chat")),
+          BottomNavyBarItem(
+              icon: SvgPicture.asset("assets/icons/notification.svg"),
+              title: Text("Notification")),
+          BottomNavyBarItem(
+              icon: SvgPicture.asset("assets/icons/user.svg"),
+              title: Text("Account")),
+        ],
+        onItemSelected: (index) => setState(() => this.indexScreen = index));
+  }
 }
---
+++ lib/screens/account/account-screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AccountScreen extends StatelessWidget {
+  const AccountScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        child: Text("this is account screen "),
+      ),
+    );
+  }
+}
