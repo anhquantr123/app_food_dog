b157bd2ad8e7a51d86651ac7a6e75753de2e20be
NGUYỄN VĂN QUÂN <57479794+anhquantr123@users.noreply.github.com>
2022-01-11 22:42:33 +0700

    fix all app

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const primaryColor = Color(0xFFff0000);
+const primaryColor = Color(0xFFEF7C91);
 const sencondColor = Color(0xFFB5BFD0);
 const textColorBlack = Color(0xFF000000);
 const textColorLightBlack = Color(0xFF363333);
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:app_food/screens/account/account-screen.dart';
 import 'package:app_food/screens/chat/chat-screen.dart';
 import 'package:app_food/screens/home/home_screen.dart';
 import 'package:app_food/screens/notification/notification-screen.dart';
-import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -17,58 +17,56 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<Map<String, dynamic>> _listIcons = [
+    {
+      "icon": "assets/icons/home.svg",
+      "lable": "Home",
+    },
+    {
+      "icon": "assets/icons/chat.svg",
+      "lable": "Chat",
+    },
+    {"icon": "assets/icons/notification.svg", "lable": "Notification"},
+    {"icon": "assets/icons/user.svg", "lable": "Account"},
+  ];
+
+  List<Widget> _listScreens = [
+    HomeScreen(),
+    ChatScreen(),
+    NotificationScreen(),
+    AccountScreen(),
+  ];
   int indexScreen = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "App Food Dog",
-      theme: ThemeData(
-          primaryColor: primaryColor,
-          scaffoldBackgroundColor: Colors.white,
-          textTheme: TextTheme(
-              bodyText1: TextStyle(color: textColorBlack),
-              bodyText2: TextStyle(color: textColorLightBlack))),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Screen(),
-        bottomNavigationBar: bottomTapNavigation(),
-      ),
-    );
-  }
-
-  // ignore: non_constant_identifier_names
-  Screen() {
-    switch (indexScreen) {
-      case 0:
-        return HomeScreen();
-      case 1:
-        return ChatScreen();
-      case 2:
-        return NotificationScreen();
-      case 3:
-        return AccountScreen();
-      default:
-        return HomeScreen();
-    }
-  }
-
-  Widget bottomTapNavigation() {
-    return BottomNavyBar(
-        selectedIndex: indexScreen,
-        items: [
-          BottomNavyBarItem(
-              icon: SvgPicture.asset("assets/icons/home.svg"),
-              title: Text("Home")),
-          BottomNavyBarItem(
-              icon: SvgPicture.asset("assets/icons/chat.svg"),
-              title: Text("Chat")),
-          BottomNavyBarItem(
-              icon: SvgPicture.asset("assets/icons/notification.svg"),
-              title: Text("Notification")),
-          BottomNavyBarItem(
-              icon: SvgPicture.asset("assets/icons/user.svg"),
-              title: Text("Account")),
-        ],
-        onItemSelected: (index) => setState(() => this.indexScreen = index));
+        title: "App Food Dog",
+        theme: ThemeData(
+            primaryColor: primaryColor,
+            scaffoldBackgroundColor: Colors.white,
+            textTheme: TextTheme(
+                bodyText1: TextStyle(color: textColorBlack),
+                bodyText2: TextStyle(color: textColorLightBlack))),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: _listScreens[indexScreen],
+          bottomNavigationBar: BottomNavigationBar(
+            showSelectedLabels: true,
+            currentIndex: indexScreen,
+            elevation: 0,
+            items: [
+              ...List.generate(
+                  _listIcons.length,
+                  (index) => BottomNavigationBarItem(
+                      icon: SvgPicture.asset(_listIcons[index]['icon']),
+                      label: _listIcons[index]['lable']))
+            ],
+            onTap: (index) {
+              setState(() {
+                indexScreen = index;
+              });
+            },
+          ),
+        ));
   }
 }
